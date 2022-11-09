6a279a845b05f5b00d9eb43e080f70f5e59a7149
Todd Yandell <tyandell@gmail.com>
2022-11-08 20:40:18 -0600

    Fix system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,12 @@
 
 require "test_helper"
 
+Capybara.register_driver :selenium_chrome_headless do |app|
+  capabilities = Selenium::WebDriver::Chrome::Options.new
+  capabilities.add_argument "--headless"
+  Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities:)
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium_chrome_headless
 end
